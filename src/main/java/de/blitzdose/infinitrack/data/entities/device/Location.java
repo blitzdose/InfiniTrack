e1319dc426ef85cd61542eb2405da459e8327624
Christian Zäske <blitzdose@gmail.com>
2023-04-14 00:08:32 +0200

    Changed GPSParser and Location

--- src/main/java/de/blitzdose/infinitrack/data/entities/device/Location.java
+++ src/main/java/de/blitzdose/infinitrack/data/entities/device/Location.java
@@ -1,79 +1,106 @@
 package de.blitzdose.infinitrack.data.entities.device;
 
 import de.blitzdose.infinitrack.data.entities.AbstractEntity;
+import de.blitzdose.infinitrack.gps.GPSParser;
 
 import javax.persistence.Entity;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.time.Instant;
+import java.util.HexFormat;
 
 @Entity
 public class Location extends AbstractEntity {
     private long timestamp;
-    private int satelliteCount;
-    private double longitude;
-    private double latitude;
-    private double altitude;
-    private double speed;
-    private double pdop;
+    private float latitude;
+    private float longitude;
+    private float speed;
+    private short satelliteCount;
+    private float altitude;
+    private float pdop;
+
+    public static Location parsePayload(String payloadHex) throws IOException {
+        byte[] payload = HexFormat.of().parseHex(payloadHex);
+        ByteArrayInputStream payloadStream = new ByteArrayInputStream(payload);
+
+        GPSParser gpsParser = new GPSParser(payloadStream);
+
+        Location location = new Location();
+        location.setTimestamp(gpsParser.getTimestamp());
+        location.setLatitude(gpsParser.getLatitude());
+        location.setLongitude(gpsParser.getLongitude());
+        location.setSpeed(gpsParser.getSpeed());
+        location.setSatelliteCount(gpsParser.getSatelliteCount());
+        location.setAltitude(gpsParser.getAltitude());
+        location.setPdop(gpsParser.getPdop());
+
+        return location;
+    }
 
     public long getTimestamp() {
         return timestamp;
     }
 
-    public Location setTimestamp(long timestamp) {
+    public void setTimestamp(long timestamp) {
         this.timestamp = timestamp;
-        return this;
     }
 
-    public double getLongitude() {
-        return longitude;
-    }
-
-    public Location setLongitude(double longitude) {
-        this.longitude = longitude;
-        return this;
-    }
-
-    public double getLatitude() {
+    public float getLatitude() {
         return latitude;
     }
 
-    public Location setLatitude(double latitude) {
+    public void setLatitude(float latitude) {
         this.latitude = latitude;
-        return this;
     }
 
-    public int getSatelliteCount() {
-        return satelliteCount;
+    public float getLongitude() {
+        return longitude;
     }
 
-    public Location setSatelliteCount(int satelliteCount) {
-        this.satelliteCount = satelliteCount;
-        return this;
+    public void setLongitude(float longitude) {
+        this.longitude = longitude;
     }
 
-    public double getAltitude() {
-        return altitude;
-    }
-
-    public Location setAltitude(double altitude) {
-        this.altitude = altitude;
-        return this;
-    }
-
-    public double getSpeed() {
+    public float getSpeed() {
         return speed;
     }
 
-    public Location setSpeed(double speed) {
+    public void setSpeed(float speed) {
         this.speed = speed;
-        return this;
     }
 
-    public double getPdop() {
+    public short getSatelliteCount() {
+        return satelliteCount;
+    }
+
+    public void setSatelliteCount(short satelliteCount) {
+        this.satelliteCount = satelliteCount;
+    }
+
+    public float getAltitude() {
+        return altitude;
+    }
+
+    public void setAltitude(float altitude) {
+        this.altitude = altitude;
+    }
+
+    public float getPdop() {
         return pdop;
     }
 
-    public Location setPdop(double pdop) {
+    public void setPdop(float pdop) {
         this.pdop = pdop;
-        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "Timestamp (UTC): " + Instant.ofEpochMilli(timestamp).toString() +
+                ", Latitude: " + latitude +
+                ", Longitude: " + longitude +
+                ", Speed: " + speed +
+                ", Satellites: " + satelliteCount +
+                ", Altitude: " + altitude +
+                ", PDOP: " + pdop;
     }
 }
--- src/main/java/de/blitzdose/infinitrack/gps/GPSParser.java
+++ src/main/java/de/blitzdose/infinitrack/gps/GPSParser.java
@@ -1,76 +1,17 @@
 package de.blitzdose.infinitrack.gps;
 
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.ByteBuffer;
-import java.util.Arrays;
-import java.util.HexFormat;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 
 public class GPSParser {
 
-    private int hours = 0;
-    private int minutes = 0;
-    private int seconds = 0;
+    InputStream payloadStream;
 
-    private int year = 0;
-    private int month = 0;
-    private int day = 0;
-
-    private float latitude = 0F;
-    private float longitude = 0f;
-
-    private int satellites = 0;
-
-    private float altitude = 0F;
-
-    private float pdop = 0F;
-
-    public GPSParser() {
-
-    }
-
-    // TODO: Direkt zu Location machen
-    public void parsePayload(String payloadHex) throws IOException {
-        payloadHex = "01517f17040a42420f300041111fcc0000064398800040e47ae1";
-        byte[] payload = HexFormat.of().parseHex(payloadHex);
-
-        ByteArrayInputStream payloadStream = new ByteArrayInputStream(payload);
-
-        byte[] timestampBytes = payloadStream.readNBytes(3);
-        long timestamp = getLongFromBytes(timestampBytes);
-
-        this.hours = (int) (timestamp / 3600);
-        timestamp = timestamp - (this.hours * 3600L);
-        this.minutes = (int) (timestamp / 60);
-        timestamp = timestamp - (this.minutes * 60L);
-        this.seconds = (int) timestamp;
-
-        byte[] yearBytes = payloadStream.readNBytes(1);
-        byte[] monthBytes = payloadStream.readNBytes(1);
-        byte[] dayBytes = payloadStream.readNBytes(1);
-
-        this.year = getIntFromBytes(yearBytes) + 2000;
-        this.month = getIntFromBytes(monthBytes);
-        this.day = getIntFromBytes(dayBytes);
-
-        byte[] latitudeBytes = payloadStream.readNBytes(4);
-        byte[] isN = payloadStream.readNBytes(1);
-
-        this.latitude = getFloatFromBytes(latitudeBytes);
-
-        byte[] longitudeBytes = payloadStream.readNBytes(4);
-        byte[] isE = payloadStream.readNBytes(1);
-
-        this.longitude = getFloatFromBytes(longitudeBytes);
-
-        byte[] satellitesBytes = payloadStream.readNBytes(1);
-        this.satellites = getIntFromBytes(satellitesBytes);
-
-        byte[] altitudeBytes = payloadStream.readNBytes(4);
-        this.altitude = getFloatFromBytes(altitudeBytes);
-
-        byte[] pdopBytes = payloadStream.readNBytes(4);
-        this.pdop = getFloatFromBytes(pdopBytes);
+    public GPSParser(InputStream payloadStream) {
+        this.payloadStream = payloadStream;
     }
 
     private float getFloatFromBytes(byte[] src) {
@@ -85,6 +26,14 @@ public class GPSParser {
         return allocateByteBuffer(src, 8).getLong();
     }
 
+    private short getShortFromBytes(byte[] src) {
+        return allocateByteBuffer(src, 2).getShort();
+    }
+
+    private boolean getBooleanFromBytes(byte[] src) {
+        return allocateByteBuffer(src, 1).get() == 0x01;
+    }
+
     private ByteBuffer allocateByteBuffer(byte[] src, int size) {
         ByteBuffer byteBuffer = ByteBuffer.allocate(size);
         byteBuffer.put(new byte[size-src.length]);
@@ -93,47 +42,65 @@ public class GPSParser {
         return byteBuffer;
     }
 
-    public int getHours() {
-        return hours;
+    public long getTimestamp() throws IOException {
+        byte[] timestampBytes = payloadStream.readNBytes(3);
+        long payloadTimestamp = getLongFromBytes(timestampBytes);
+
+        int hours = (int) (payloadTimestamp / 3600);
+        payloadTimestamp = payloadTimestamp - (hours * 3600L);
+        int minutes = (int) (payloadTimestamp / 60);
+        payloadTimestamp = payloadTimestamp - (minutes * 60L);
+        int seconds = (int) payloadTimestamp;
+
+        byte[] yearBytes = payloadStream.readNBytes(1);
+        byte[] monthBytes = payloadStream.readNBytes(1);
+        byte[] dayBytes = payloadStream.readNBytes(1);
+
+        int year = getIntFromBytes(yearBytes) + 2000;
+        int month = getIntFromBytes(monthBytes);
+        int day = getIntFromBytes(dayBytes);
+
+        LocalDateTime localDateTime = LocalDateTime.of(year, month, day, hours, minutes, seconds);
+        return localDateTime.toInstant(ZoneOffset.UTC).toEpochMilli();
     }
 
-    public int getMinutes() {
-        return minutes;
+    public float getLatitude() throws IOException {
+        return parseCoordinate();
     }
 
-    public int getSeconds() {
-        return seconds;
+    public float getLongitude() throws IOException {
+        return parseCoordinate();
     }
 
-    public int getYear() {
-        return year;
+    private float parseCoordinate() throws IOException {
+        byte[] coordinateBytes = payloadStream.readNBytes(4);
+        byte[] isPositiveBytes = payloadStream.readNBytes(1);
+        boolean isPositive = getBooleanFromBytes(isPositiveBytes);
+
+        float coordinate = getFloatFromBytes(coordinateBytes);
+        if (!isPositive) {
+            coordinate = -coordinate;
+        }
+        return coordinate;
     }
 
-    public int getMonth() {
-        return month;
+    public float getSpeed() throws IOException {
+        byte[] speedBytes = payloadStream.readNBytes(4);
+        return getFloatFromBytes(speedBytes);
     }
 
-    public int getDay() {
-        return day;
+    public short getSatelliteCount() throws IOException {
+        byte[] satellitesBytes = payloadStream.readNBytes(1);
+        return getShortFromBytes(satellitesBytes);
     }
 
-    public float getLatitude() {
-        return latitude;
+    public float getAltitude() throws IOException {
+        byte[] altitudeBytes = payloadStream.readNBytes(4);
+        return getFloatFromBytes(altitudeBytes);
     }
 
-    public float getLongitude() {
-        return longitude;
-    }
-
-    public int getSatellites() {
-        return satellites;
-    }
-
-    public float getAltitude() {
-        return altitude;
-    }
-
-    public float getPdop() {
-        return pdop;
+    public float getPdop() throws IOException {
+        byte[] pdopBytes = payloadStream.readNBytes(4);
+        return getFloatFromBytes(pdopBytes);
     }
 }
